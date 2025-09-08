e512336ff10a544472015eecf271fa584ad37bb3
kobe bryan Corona <coronakobebryan_bsit@plmun.edu.ph>
2025-09-08 09:16:13 +0800

    Submitting lab 2

--- 02_files_check.sh
+++ 02_files_check.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-declare -a files
 directory="university_db"
-SQL_FILES=("create_and_use_db.sql" "create_students_table.sql" "alter_students_table.sql") # Array of filenames
+files=("create_and_use_db.sql" "create_students_table.sql" "alter_students_table.sql") # Array of filenames
 
-for file in "${files[@]}"; do  # Important: Quote "${files[@]}"
-filepath="$directory/$file" # Construct the full path
-if [ -f "$filepath" ]; then
+for file in "${files[@]}"; do
+  filepath="$directory/$file"
+  if [ -f "$filepath" ]; then
     echo "$filepath exists"
-else
-    echo "$filepath does not exist"
-fi
+  else
+    echo "Error: $filepath does not exist"
+    exit 1
+  fi
 done
+
+exit 0
