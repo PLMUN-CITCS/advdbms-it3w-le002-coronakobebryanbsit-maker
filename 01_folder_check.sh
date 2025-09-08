cafc00a0d44a051107d24ef2477ea8550f3f04ee
kobe bryan Corona <coronakobebryan_bsit@plmun.edu.ph>
2025-09-08 09:14:39 +0800

    Submitting lab 2

--- 01_folder_check.sh
+++ 01_folder_check.sh
@@ -3,7 +3,9 @@
 directory="university_db"
 
 if [ -d "$directory" ]; then
-echo "Folder exists"
+  echo "Folder '$directory' exists."
+  exit 0
 else
-echo "Folder does not exist"
+  echo "Error: Folder '$directory' does not exist."
+  exit 1
 fi
